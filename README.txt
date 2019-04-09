3c8be380314cf3f05440051368981004441115f0
Tristan O'Neill <troneill2015@gmail.com>
2019-04-09 14:43:59 +0900

    Original

---
+++ README.txt
@@ -0,0 +1,19 @@
+To use:
+- move this folder to desired location.
+- add the following lines to .bashrc file
+export LOGPATH="/path/to/log/folder/"
+source {$LOGPATH}log.sh
+- then add the following line to .vimrc file
+set backupcopy=yes
+- this file is not normally created by default.
+  you may have to create it to set user settings
+  in this way.
+- without this step, vim may overwrite your logs
+ - this is because by default vim usually 
+   overwrites your old file with a new file, and
+   because the logs are stored as "extended file
+   attributes" the logs are lost
+ - care should be taken with other text editors
+   as this problem could also occur with those
+- if you fail to do this, there is a log kept of
+  every log every committed stored in $LOGPATH
---
+++ log.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+ 
+logset() {
+  TEMP_CURR=recent.log
+  TEMP_PREV=all.log
+  if [ -f "$1" ]
+  then
+    if [ -f $LOGPATH$TEMP_CURR ]
+    then
+      cat "FROM UNKNOWN FILE, UNKNOWN DATE" >> "$LOGPATH$TEMP_PREV"
+      cat "$LOGPATH$TEMP_CURR" >> "$LOGPATH$TEMP_PREV"
+      rm "$LOGPATH$TEMP_CURR"
+    fi
+    touch $LOGPATH$TEMP_CURR
+    getfattr --only-values -e text -n user.log $1 > $LOGPATH$TEMP_CURR
+    vim $LOGPATH$TEMP_CURR
+
+    echo "##########################################################################################################" >> "$LOGPATH$TEMP_PREV"
+    readlink -f $1 >> "$LOGPATH$TEMP_PREV"
+
+    setfattr -n user.logUpdateTime -v "$(date)" $1
+    date >> "$LOGPATH$TEMP_PREV"
+
+    desc=$(<$LOGPATH$TEMP_CURR)
+    setfattr -n user.log -v "$desc" $1
+    cat "$LOGPATH$TEMP_CURR" >> "$LOGPATH$TEMP_PREV"
+
+    rm "$LOGPATH$TEMP_CURR"
+  else
+    echo 'File does not exist'
+  fi
+}
+
+log() {
+  if [ -f "$1" ]
+  then
+    echo 'This log was last updated on:'
+    getfattr --only-values -e text -n user.logUpdateTime $1
+    echo ''
+    getfattr --only-values -e text -n user.log $1
+    echo ''
+  else
+    echo 'File does not exist'
+  fi
+}
